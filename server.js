05d35ee6693195fa1c1b846ebcde9d3dfdd06ad4
MartinQR <quirozrmartin@gmail.com>
2024-11-23 10:11:37 -0600

    fix env

--- server.js
+++ server.js
@@ -17,10 +17,13 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
-
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error("Error connecting to database:", err);
+    process.exit(1);
+  }
   console.log("Successful connection to database");
 });
 
